114043a10a6fdce04bda344f7f94cd3ba70e9218
codets1989 <ankitmahto75@gmail.com>
2026-02-28 15:46:33 -0600

    fstack

---
+++ fstack.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cstack.h"
+
+void init_fstack(fstack *s) {
+    *s = NULL;
+}
+
+boolean fis_empty(fstack s) {
+    if (s == NULL)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+boolean fis_full(void) {
+    fstack temp;
+    temp = (fstack) malloc(sizeof(struct stacknode));
+    if (temp == NULL)
+        return TRUE;
+    else {
+        free(temp);
+        return FALSE;
+    }
+}
+void fpush(fstack *s, char x) {
+    fstack temp;
+    temp = (fstack) malloc(sizeof(struct fstacknode));
+    temp -> data = x;
+    temp -> next = (*s);
+    (*s) = temp;
+}
+char fpop(fstack *s) {
+    fstack temp;
+    float data_popped;
+    temp = *s;
+    data_popped = temp->data;
+    *s = temp->next;
+    free (temp);
+    return data_popped;
+}
+void print_fstack(fstack s) {
+    if (!fis_empty(s)){
+        printf("%f \n", s->data);
+        print_fstack(s->next);
+    }
+}
---
+++ fstack.h
@@ -0,0 +1,18 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include "boolean.h"
+
+typedef struct stacknode {
+    float  data;
+    struct stacknode *next;
+} *fstack;
+
+void init_fstack(fstack *);
+boolean fis_empty(fstack);
+boolean fis_full(void);
+void fush(fstack *, char);
+int fpop(fstack *);
+void print_fstack(fstack s);
+
+#endif
