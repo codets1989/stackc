08f3cceaf8be296d4855711736cc462ad727e337
codets1989 <ankitmahto75@gmail.com>
2026-04-28 15:49:35 -0500

    menu update

---
+++ stackmainmenu.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "stack.h"
+
+int menu(void);
+
+int main(void) {
+    stack top;
+    int data_item;
+    int selection;
+
+    init_stack(&top);
+    selection = menu();
+
+    while (selection != 4) {
+        switch(selection) {
+
+            case 1:
+                if (!is_full()) {
+                    printf("Enter number: ");
+                    scanf("%d", &data_item);
+                    push(&top, data_item);
+                } else {
+                    printf("Stack is full\n");
+                }
+                break;
+
+            case 2:
+                if (!is_empty(top)) {
+                    data_item = pop(&top);
+                    printf("%d popped\n", data_item);
+                } else {
+                    printf("Stack is empty\n");
+                }
+                break;
+
+            case 3:
+                if (!is_empty(top)) {
+                    print_stack(top);
+                } else {
+                    printf("Stack is empty\n");
+                }
+                break;
+
+            default:
+                printf("Not a valid selection\n");
+        }
+
+        selection = menu();
+    }
+
+    return 0;
+}
